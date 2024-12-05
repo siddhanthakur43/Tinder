61b66d236e22052a6da1d26eccf36a0a86b5b93b
siddhanthakur43 <siddhantthakur43@gmail.com>
2024-12-05 23:18:14 +0530

    exploring put patch post calls

--- src/app.js
+++ src/app.js
@@ -2,20 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-    res.send("Welcome to hello");
+app.get('/user', (req, res) => {
+    res.send([{ firstName: 'Tanya', lastName: 'Dutt' }, { firstName: 'Anushka', lastName: 'Mishra' }])
+});
+
+app.post('/user', (req, res) => {
+    res.send('Data Saved Successfully');
+});
+
+app.put('/user', (req, res) => {
+    res.send('Data Updated Successfully via put');
+});
+
+app.patch('/user', (req, res) => {
+    res.send('Data Updated Successfully via patch');
+});
+
+app.delete('/user', (req, res) => {
+    res.send('Data Deleted Successfully');
 });
 
 app.use('/test/123', (req, res) => {
     res.send('test 123');
-})
-
-app.use("/test", (req, res) => {
-    res.send("Welcome to test");
-});
-
-app.use('/',(req, res) => {
-    res.send("Hello Guys!! Welcome to tinder");
 });
 
 app.listen(8080, () => {
